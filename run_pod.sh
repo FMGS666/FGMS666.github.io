b38eebc152d0f50783f03f612bc1c2323b99ce58
FMGS666 <b00794799@essec.edu>
2023-12-21 20:55:09 +0100

    adding alias for conda

--- run_pod.sh
+++ run_pod.sh
@@ -4,6 +4,7 @@ mkdir -p ~/miniconda3;
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh;
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3;
 rm -rf ~/miniconda3/miniconda.sh;
+alias conda="~/miniconda/bin/conda";
 # Initializing kaggle user
 mkdir ~/.kaggle;
 echo "{'username':'fmgsf12','key':'d1885b9fd9a3ed92a2a4fd70de6f5a7e'}" > ~/.kaggle/kaggle.json;
@@ -27,5 +28,5 @@ conda env create -f env.yml;
 conda run -n blood-vessel-seg kaggle competitions download -c blood-vessel-segmentation;
 unzip blood-vessel-segmentation.zip -d data;
 rm blood-vessel-segmentation.zip;
-# Creating volumes 
+# Creating volumes
 conda run -n python3 -m bv-seg sample --n-samples 5 --context-length 100;
