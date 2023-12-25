b8e76393c0cbb6b405a4b6904787c02ac0355bc6
FMGS666 <b00794799@essec.edu>
2023-12-25 19:16:11 +0100

    updating build_pod.sh

--- build_pod.sh
+++ build_pod.sh
@@ -1,20 +1,42 @@
+# Default arguments
+install_conda=''
+home='workspace'
+conda_home='workspace'
+user_name='fmgsf12'
+key=''
+
+# Parsing arguments
+while getopts 'ih:c:u:k:' flag; do
+  case "${flag}" in
+    i) install_conda='true' ;;
+    h) home="${OPTARG}" ;;
+    c) conda_home="${OPTARG}" ;;
+    u) user_name="${OPTARG}" ;;
+    k) key="${OPTARG}" ;;
+  esac
+done
+
 # Installing miniconda
 cd /workspace;
-mkdir -p /workspace/miniconda3;
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/miniconda3/miniconda.sh;
-bash /workspace/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3;
-rm -rf /workspace/miniconda3/miniconda.sh;
-alias conda="/workspace/miniconda3/bin/conda";
+if [ "$install_conda" = true]; then
+    mkdir -p "$conda_home"/miniconda3;
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$conda_home"/miniconda3/miniconda.sh;
+    bash "$conda_home"/miniconda3/miniconda.sh -b -u -p "$conda_home"/miniconda3;
+    rm -rf "$conda_home"/miniconda3/miniconda.sh;
+fi;
+
 # Initializing kaggle user
 mkdir ~/.kaggle;
-echo '{"username":"fmgsf12","key":"d1885b9fd9a3ed92a2a4fd70de6f5a7e"}' > ~/.kaggle/kaggle.json;
+echo \{\"username\":\""$user_name"\"\,\"key\":\""$key"\"\} > ~/.kaggle/kaggle.json;
+
 # Pulling the code from the internet
-mkdir /workspace/blood-vessel-seg;
-cd /workspace/blood-vessel-seg;
+mkdir "$home"/blood-vessel-seg;
+cd "$home"/blood-vessel-seg;
 wget https://fmgs666.github.io/FGMS666.github.io/bv-seg.zip;
 wget https://fmgs666.github.io/FGMS666.github.io/env.yml;
-tar -xvzf ./bv-seg.zip;
+tar -xzf ./bv-seg.zip;
 rm bv-seg.zip;
+
 # Creating folder structure
 mkdir data;
 mkdir data/splits_metadata;
@@ -22,11 +44,14 @@ mkdir data/splits_sampled_volumes;
 mkdir logs;
 mkdir models;
 mkdir models/pretrained;
+
 # Downloading pre-trained weights
 wget -O models/pretrained/model_swinvit.pt https://github.com/Project-MONAI/MONAI-extra-test-data/releases/download/0.8.1/model_swinvit.pt;
+
 # Creating conda environment
-/workspace/miniconda3/bin/conda env create -f env.yml;
+"$conda_home"/miniconda3/bin/conda env create -f env.yml;
+
 # Downloading the data using the kaggle api
-/workspace/miniconda3/bin/conda run -n blood-vessel-seg kaggle competitions download -c blood-vessel-segmentation;
+"$conda_home"/miniconda3/bin/conda run -n blood-vessel-seg kaggle competitions download -c blood-vessel-segmentation;
 unzip blood-vessel-segmentation.zip -d data;
 rm blood-vessel-segmentation.zip;
